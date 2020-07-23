bd6e2fcf01276c9202fd67369025b5f0b513315b
andredavid1 <andredavid1@yahoo.com.br>
2020-07-23 14:12:54 -0300

    build

---
+++ build/precache-manifest.ad9a090a932dded9bbc62ea78ae2e508.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d1c3d5e41c075a27b098dcb6aac4ebc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8591d30ccac126c5aaa",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e21582deb8b741c34202",
+    "url": "/static/css/main.a1f298ff.chunk.css"
+  },
+  {
+    "revision": "c8591d30ccac126c5aaa",
+    "url": "/static/js/2.ec11c5ac.chunk.js"
+  },
+  {
+    "revision": "44d25822c9afc21e09075ceaf5b496f4",
+    "url": "/static/js/2.ec11c5ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e21582deb8b741c34202",
+    "url": "/static/js/main.b3e8150d.chunk.js"
+  },
+  {
+    "revision": "c627aa68045578994b8b",
+    "url": "/static/js/runtime-main.e938b3c0.js"
+  },
+  {
+    "revision": "d321fa78bb7190a8ca7e14213ef63203",
+    "url": "/static/media/Aileron-Regular.d321fa78.otf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "e00e2a77dd88a8fe75573a5d993af76a",
+    "url": "/static/media/Ubuntu-Bold.e00e2a77.ttf"
+  },
+  {
+    "revision": "0980debea9965b6f7581aa34046a03c5",
+    "url": "/static/media/cel.0980debe.jpeg"
+  },
+  {
+    "revision": "08a8aa82727e032eed2c57204e4389ab",
+    "url": "/static/media/diva.08a8aa82.png"
+  },
+  {
+    "revision": "8ce47fd872187bfa8d11505b3bd8944e",
+    "url": "/static/media/instagram.8ce47fd8.png"
+  },
+  {
+    "revision": "ce30bb499139db24091772391f7de552",
+    "url": "/static/media/logo.ce30bb49.gif"
+  },
+  {
+    "revision": "8feff93941a2425f1d7b98118625a17d",
+    "url": "/static/media/user.8feff939.png"
+  }
+]);
